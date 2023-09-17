93281968dd7a97566a636db719ec86a7b42ca926
Al-Qema <rana.o.hourani@gamil.com>
2023-09-17 18:30:18 +0300

    second commit

---
+++ test3.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    total=x+y
+    print(total)
+add(x=10,y=20)
+
+def sub(x,y):
+    total=x-y
+    print(total)
+sub(x=10,y=20)
+def div(x,y):
+    total=x/y
+    print(total)
+div(x=10,y=20)
+def mul(x,y):
+    total=x*y
+    print(total)
+add(x=10,y=20)
