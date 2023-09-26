9817bfcd79c53df17834e500607cddb4c046b802
Al-Qema <rana.o.hourani@gamil.com>
2023-09-26 17:44:44 +0300

    5 commit

---
+++ Task 5.py
@@ -0,0 +1,24 @@
+
+
+d1= {"Name" : "Ahmed" , "Age" : 23}
+d2 = {"City" : "Gaza", "Gender" : "Male"}
+d3 = {}
+for d in (d1, d2): d3.update(d)
+print(d3)
+Concatenate_Dicts = {'Name' : 'Ram', 'Age' : 23, 'City' : 'Salem', 'Gender': 'Male'}
+
+while True:
+ try:
+    length=int(input("enter nummber1"))
+    breadth=int(input("enter nummber2"))
+    area=length*breadth
+
+    if area < 0 :
+        print("invalid input , area must be bigger than zero ")
+    else:
+        break
+ except:
+     print("invalid input")
+
+area=length*breadth
+print(area)
